28044d5fc9785c8e265b4ec907d993fa5d358948
Arash <arash.mjr@gmail.com>
2022-12-05 14:36:46 +0330

    First commit

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,30 +1,10 @@
 from django.urls import include, path
-from src.api.views.reminders import (
-    CreateReminderAPIView,
-    GetReminderListAPIView,
-    ReminderAPIView,
-)
 
-accounts_url_patterns = [
-    path(
-        "<str:user_id>/reminders/",
-        GetReminderListAPIView.as_view(),
-        name="reminder_list",
-    ),
-]
+accounts_url_patterns = []
 
-reminders_url_patterns = [
-    path(
-        "",
-        CreateReminderAPIView.as_view(),
-        name="create_reminder",
-    ),
-    path("<str:id>/", ReminderAPIView.as_view(), name="reminder"),
-]
 
 V_0_0_0_urlpatterns = [
-    path("users/", include(accounts_url_patterns)),
-    path("reminders/", include(reminders_url_patterns)),
+    path("accounts/", include(accounts_url_patterns)),
 ]
 
 urlpatterns = [
--- src/errors/error_enum.py
+++ src/errors/error_enum.py
@@ -1,6 +1,2 @@
 class ErrorEnum:
-    class Reminder:
-        INVALID_LIMIT_AND_OFFSET = "Reminder_INVALID_LIMIT_AND_OFFSET"
-
-    class Service:
-        INVALID_USER_ID = "SERVICE_INVALID_USER_ID"
+    pass
